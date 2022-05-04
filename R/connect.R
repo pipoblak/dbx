7f24cc3e68f1005afb4ff089615c900215d686a6
Felipe Ramos da Silva <pipoblakgamer@gmail.com>
2022-05-04 15:48:01 -0300

    Update connect.R

--- R/connect.R
+++ R/connect.R
@@ -150,6 +150,7 @@ dbxConnect <- function(url=NULL, adapter=NULL, storage_tz=NULL, variables=list()
     DBI::dbExecute(conn, "SET timezone TO 'UTC'")
   } else if (isRMySQL(conn) || isODBCMySQL(conn)) {
     DBI::dbExecute(conn, "SET time_zone = '+00:00'")
+    DBI::dbExecute(conn, "SET NAMES utf8")
   }
 
   for (k in names(variables)) {
